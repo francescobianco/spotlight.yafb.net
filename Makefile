d675537597ec89ae141314be2eab849f78addb85
francescobianco <info.francescobianco@gmail.com>
2024-09-17 00:51:33 +0200

    Guida alla consultazione

--- Makefile
+++ Makefile
@@ -1,2 +1,10 @@
-test:
+
+
+build:
 	bash contrib/build-pdf.sh
+
+test:
+	docker run --rm \
+           --volume "$$(pwd):/data" \
+           --user $$(id -u):$$(id -g) \
+           pandoc/extra contrib/example.md -o contrib/example.pdf --template eisvogel --listings
--- contrib/build-pdf.sh
+++ contrib/build-pdf.sh
@@ -3,10 +3,13 @@
 # Trova tutti i file .md nella cartella archive e li converte in PDF
 find archive -name "*.md" | while read -r file; do
     # Esegui il container Docker con pandoc per ogni file trovato
-    echo docker run --rm \
+    pdf=$(dirname "$file")/$(basename "$file" .md).pdf
+    echo $file $pdf
+
+     docker run --rm \
         --volume "$(pwd):/data" \
         --user "$(id -u):$(id -g)" \
-        pandoc/extra pandoc "$file" -o "$(dirname "$file")/$(basename "$file" .md).pdf" \
+        pandoc/extra "$file" -o "$pdf" \
         --template eisvogel --listings
 
     echo "Convertito: $file in $(dirname "$file")/$(basename "$file" .md).pdf"

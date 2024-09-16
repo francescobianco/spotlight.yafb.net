90d12de77398cee19d2e4cb7dddbda911c2a5bfd
francescobianco <info.francescobianco@gmail.com>
2024-09-17 01:35:30 +0200

    Guida alla consultazione

--- contrib/build-pdf.sh
+++ contrib/build-pdf.sh
@@ -4,13 +4,16 @@
 find archive -name "*.md" | while read -r file; do
     # Esegui il container Docker con pandoc per ogni file trovato
     pdf=$(dirname "$file")/$(basename "$file" .md).pdf
-    echo $file $pdf
+    html=$(dirname "$file")/$(basename "$file" .md).html
+       echo $file $pdf
 
      docker run --rm \
         --volume "$(pwd):/data" \
         --user "$(id -u):$(id -g)" \
         pandoc/extra "$file" -o "$pdf" \
         --template eisvogel --listings
+
+  pandoc $file -o $html
 
     echo "Convertito: $file in $(dirname "$file")/$(basename "$file" .md).pdf"
 done
